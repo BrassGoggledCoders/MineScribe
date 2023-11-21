85f0d5df50a549bd15fdaf8b28df42ca82341f04
Skylar <skylar.som@gmail.com>
2023-11-20 23:19:22 -0500

    Add script info to ScriptHandler

--- Editor/src/main/java/xyz/brassgoggledcoders/minescribe/editor/javascript/ScriptHandler.java
+++ Editor/src/main/java/xyz/brassgoggledcoders/minescribe/editor/javascript/ScriptHandler.java
@@ -2,11 +2,13 @@ package xyz.brassgoggledcoders.minescribe.editor.javascript;
 
 import com.google.common.base.Suppliers;
 import org.graalvm.polyglot.*;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.io.Closeable;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 public class ScriptHandler implements Closeable {
@@ -16,6 +18,7 @@ public class ScriptHandler implements Closeable {
     private final Engine engine;
     private final Context context;
     private final SLF4JBridgeHandler handler;
+    private final AtomicReference<String> currentScript;
 
     public ScriptHandler() {
         this.engine = Engine.newBuilder()
@@ -27,6 +30,7 @@ public class ScriptHandler implements Closeable {
                 .putMember("minescribe", helper);
         this.context.getBindings("js")
                 .putMember("validationHelper", helper.validationHelper);
+        this.currentScript = new AtomicReference<>();
     }
 
     private Context buildContext(Engine engine) {
@@ -38,13 +42,19 @@ public class ScriptHandler implements Closeable {
                 .build();
     }
 
+    @Nullable
+    public String getCurrentScript() {
+        return this.currentScript.get();
+    }
+
 
     public void runScript(String fileName, String fileContents) {
         try {
             Source source = Source.create("js", fileContents);
-
+            this.currentScript.set(fileContents);
             this.context.parse(source)
                     .executeVoid();
+            this.currentScript.set(null);
         } catch (PolyglotException polyglotException) {
             LOGGER.error("Failed to run Script {}", fileName, polyglotException);
         }
